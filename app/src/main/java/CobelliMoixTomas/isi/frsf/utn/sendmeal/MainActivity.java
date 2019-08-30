c42add250ddc156a616f615534e12a1fd30eb6a3
Santiago Cobelli <cobellisantiago@gmail.com>
2019-08-29 21:57:20 -0300

    Version de clases - Hasta validaciones parciales.

--- app/src/main/java/CobelliMoixTomas/isi/frsf/utn/sendmeal/MainActivity.java
+++ app/src/main/java/CobelliMoixTomas/isi/frsf/utn/sendmeal/MainActivity.java
@@ -3,6 +3,10 @@ package CobelliMoixTomas.isi.frsf.utn.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,108 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editTextEmail = findViewById(R.id.editTextEmail);
+        editTextEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextEmail.getText().length()==0) {
+                        editTextEmail.setError("*Campo obligatorio");
+                    }else{
+                        editTextEmail.setError(null);
+                    }
+                }
+            }
+        });
+
+        final EditText editTextPassword = findViewById(R.id.editTextPassword);
+        final EditText editTextPasswordRepeat = findViewById(R.id.editTextPasswordRepeat);
+        editTextPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextPassword.getText().length()==0){
+                        editTextPassword.setError("*Campo obligatorio");
+                    }else{
+                        editTextEmail.setError(null);
+                    }
+                }
+            }
+        });
+        editTextPasswordRepeat.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextPasswordRepeat.getText().length()==0){
+                        editTextPasswordRepeat.setError("*Campo Obligatorio");
+                    }else{
+                        if(!editTextPassword.getText().equals(editTextPasswordRepeat.getText())){
+                            editTextPasswordRepeat.setError("*Passwords no coinciden");
+                        }else{
+                            editTextPasswordRepeat.setError(null);
+                        }
+                    }
+                }
+            }
+        });
+        validarDatosTarjetaCredito();
+        validarRadioButtonGroup();
+    }
+
+    void validarDatosTarjetaCredito(){
+        final EditText editTextNumeroTarjeta = findViewById(R.id.editTextNumeroTarjeta);
+        final EditText editTextNumeroSeguridad = findViewById(R.id.editTextNumeroSeguridadTarjeta);
+        final EditText editTextDateVencimiento = findViewById(R.id.editTextDateVencimiento);
+
+        editTextNumeroTarjeta.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextNumeroTarjeta.getText().length()==0){
+                        editTextNumeroTarjeta.setError("*Campo Obligatorio");
+                    }else{
+                        editTextNumeroTarjeta.setError(null);
+                    }
+                }
+            }
+        });
+        editTextNumeroSeguridad.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextNumeroSeguridad.getText().length()==0){
+                        editTextNumeroSeguridad.setError("*Campo Obligatorio");
+                    }else{
+                        editTextNumeroSeguridad.setError(null);
+                    }
+                }
+            }
+        });
+        editTextDateVencimiento.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(editTextDateVencimiento.getText().length()==0){
+                        editTextDateVencimiento.setError("*Campo Obligatorio");
+                    }else{
+                        editTextDateVencimiento.setError(null);
+                    }
+                }
+            }
+        });
+    }
+    void validarRadioButtonGroup(){
+        final RadioGroup radioGroupTipoCuenta = findViewById(R.id.radioGroupTipoDeCuenta);
+        final RadioButton radioButtonCuentaFull = findViewById(R.id.radioButtonCuentaFull);
+        radioGroupTipoCuenta.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(!focus){
+                    if(radioGroupTipoCuenta.getCheckedRadioButtonId() == -1)radioButtonCuentaFull.setError("*Campo Obligatorio");
+                    else radioButtonCuentaFull.setError(null);
+                }
+            }
+        });
     }
 }
