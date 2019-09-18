05af46b54c42ae7ef8741724ffe17a2639efa4ca
MalenaMoix <moixmalena@gmail.com>
2019-09-18 12:51:11 -0300

    Interfaz Crear Item

---
+++ app/src/main/java/cobelli_moix_tomas/isi/frsf/utn/sendmeal/NewItem.java
@@ -0,0 +1,14 @@
+package cobelli_moix_tomas.isi.frsf.utn.sendmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+}
