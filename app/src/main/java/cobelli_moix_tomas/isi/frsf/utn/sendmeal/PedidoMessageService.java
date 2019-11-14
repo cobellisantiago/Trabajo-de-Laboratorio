da8e20b21517d752e655422029305cd0700cb7d3
MalenaMoix <moixmalena@gmail.com>
2019-11-14 18:27:20 -0300

    Dependencias Firebase

---
+++ app/src/main/java/cobelli_moix_tomas/isi/frsf/utn/sendmeal/PedidoMessageService.java
@@ -0,0 +1,6 @@
+package cobelli_moix_tomas.isi.frsf.utn.sendmeal;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PedidoMessageService extends FirebaseMessagingService {
+}
